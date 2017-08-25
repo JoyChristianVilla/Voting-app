46c02c4e4cd28207bff598fdf2f77020b9379ac4
Christian Villa <joychristianvilla@gmail.com>
2017-08-24 18:19:29 -0700

    Make chart work on vote-protected

--- public/pie-chart.js
+++ public/pie-chart.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
   //Maybe an ajax request but still really confused how that would work
   //Using options as a placeholder for the information
   var ctx = document.getElementById("chart").getContext('2d');
-  var labels = Object.keys(options);
-  var data = Object.values(options);
+  var labels = Object.keys(JSON.parse($('#chartOptions').val()));
+  var data = Object.values(JSON.parse($('#chartOptions').val()));
   var possibleColors = ["#2ecc71", "#3498db", "#95a5a6", "#9b59b6", "#f1c40f", "#e74c3c", "#34495e", "#ff6384", "#36a2eb", "#cc65fe", "#ffce56"]
   var colors = [];
   for (i = 0; i < data.length; i++) {
